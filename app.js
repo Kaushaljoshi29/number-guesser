c1d415abe66e5696706820da3b0b91e40fecb6e8
Kaushal Joshi <joshikaushal1105@gmail.com>
2022-05-14 11:34:22 +0530

    removed console.log from app.js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ let min = 1,
   winningNum = getRandomNum(min, max),
   chancesLeft = 3;
 
-console.log(winningNum)  
-
 //Grab elements from the DOM
 const gameInput = document.getElementById("guess-input");
 const gameSubmit = document.getElementById("submit-btn");
@@ -14,35 +12,32 @@ const loading = document.querySelector(".loading-gif");
 const game = document.querySelector(".game-wrapper");
 
 //Play again event handler
-game.addEventListener('mousedown', function(e){
-
-    // console.log(e.target)
-    if (e.target.value === "Play Again") {
-      window.location.reload();
-    }
-
-})
+game.addEventListener("mousedown", function (e) {
+  // console.log(e.target)
+  if (e.target.value === "Play Again") {
+    window.location.reload();
+  }
+});
 
 //Submit Event handler
 gameSubmit.addEventListener("click", function () {
   const guess = parseInt(gameInput.value);
   loading.style.display = "block";
-  errorMsg.style.display = 'none'
+  errorMsg.style.display = "none";
 
   setTimeout(checkLogic, 2000);
 
   function checkLogic() {
-    loading.style.display = 'none';
+    loading.style.display = "none";
 
     if (isNaN(guess) || guess < min || guess > max) {
       displayMessage(`Please enter a number between ${min} and ${max}`, "red");
     } else {
       //Winning case, Game Over
       if (guess === winningNum) {
-        console.log(`${guess} is correct, you won!`);
         displayMessage(`${guess} is correct. You won!`, "green");
         gameInput.disabled = true;
-        gameSubmit.value ='Play Again';
+        gameSubmit.value = "Play Again";
       }
       //Losing case, Game over
       else {
